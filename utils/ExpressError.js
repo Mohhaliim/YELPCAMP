9a966e5d9639b628e2bef39474b11a46dbe34727
Mohamed Abdelhalim <mohamed.abd-elhalim@hotmail.com>
2021-05-29 21:50:19 +0200

    first commit

---
+++ utils/ExpressError.js
@@ -0,0 +1,12 @@
+const { Error } = require("mongoose");
+
+class ExpressError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+
+module.exports = ExpressError;
